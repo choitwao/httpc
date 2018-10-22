0835b7987bd7e52682a13c41032e7ea12e8971a7
choitwao <rkevincai@hotmail.com>
2018-10-22 18:00:23 -0400

    add cli for httpf

---
+++ httpf/cli.py
@@ -0,0 +1,32 @@
+from argparse import ArgumentParser
+
+class Cli:
+
+    @staticmethod
+    def create_parser():
+        # create the main parser for CLI
+        main_parser = ArgumentParser(prog='httpf',
+                                     description='httpc is a simple file server.',
+                                     add_help=False,
+                                     conflict_handler='resolve')
+        # create a general template for GET and POST
+        main_parser.add_argument('-v',
+                                 dest='verbose',
+                                 action='store_const',
+                                 const=True,
+                                 default=False,
+                                 help='Print debugging messages.')
+        main_parser.add_argument('-p',
+                                 dest='port',
+                                 action='store',
+                                 default='8080',
+                                 help='Specifies the port number that the server will listen and serve at. Default is 8080')
+        main_parser.add_argument('-d',
+                                 dest='directory',
+                                 action='store',
+                                 help='Specifies the directory that the server will use to read/write requested files. Default is the current directory when launching the application.')
+        return main_parser
+
+
+
+
