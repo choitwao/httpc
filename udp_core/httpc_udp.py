d06fc925fdd9f8e81df84b1abd8f9d352b014b35
choitwao <rkevincai@hotmail.com>
2018-11-26 19:21:45 -0500

    ack, data, ack tested

--- udp_core/httpc_udp.py
+++ udp_core/httpc_udp.py
@@ -3,7 +3,7 @@ import ipaddress
 import socket
 from udp_core.packet import Packet
 
-
+# Packet Type: 1-SYN, 2-SYN-ACK, 3-ACK 4-DATA 5-ACK
 class HttpcUDP:
 
     def __init__(self, router_host, router_port, server_host, server_port):
@@ -11,23 +11,31 @@ class HttpcUDP:
         self.router_port = router_port
         self.server_host = server_host
         self.server_port = server_port
+        self.peer_ip = ipaddress.ip_address(socket.gethostbyname(self.server_host))
 
     def run(self):
-        peer_ip = ipaddress.ip_address(socket.gethostbyname(self.server_host))
         connection = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         # Firstly handshake with the server
         is_handshaked = False
         while not is_handshaked:
-            is_handshaked = self.send_handshake(connection, peer_ip)
+            is_handshaked = self.__send_handshake__(connection)
         # If handshaked, start sending ACK+data request
+        is_requested = False
+        response_data = None
+        retry = 0
+        while not is_requested:
+            retry += 1
+            is_requested, response_data = self.__send_request__(connection)
+        print(response_data)
         connection.close()
 
-    def send_handshake(self, connection, peer_ip):
+    def __send_handshake__(self, connection):
         success = False
         try:
-            # create sync packet, where type = 1, seq_num = 1, payload = 'SYN'
+            print('---- Handshaking ----')
+            # create SYNC packet, where type = 1, seq_num = 1, payload = 'SYN'
             print('Client: Creating SYN packet')
-            syn_packet = Packet(1, 1, peer_ip, self.server_port, 'SYN'.encode('utf-8'))
+            syn_packet = Packet(1, 1, self.peer_ip, self.server_port, 'SYN'.encode('utf-8'))
             # send the packet to the router
             print('Client: Sending SYN to router')
             connection.sendto(syn_packet.to_bytes(), (self.router_host, self.router_port))
@@ -48,6 +56,43 @@ class HttpcUDP:
         except socket.timeout:
             print('Client: No response after 1 second, handshake failed.')
         return success
+
+    def __send_request__(self, connection):
+        success = False
+        response_data = None
+        try:
+            print('---- Requesting ----')
+            # create ACK packet, where type = 3, seq_num = 3, payload = 'ACK'
+            print('Client: Creating ACK packet')
+            ack_packet = Packet(3, 3, self.peer_ip, self.server_port, 'Some data'.encode('utf-8'))
+            # send the packet to the router
+            print('Client: Sending ACK to router')
+            connection.sendto(ack_packet.to_bytes(), (self.router_host, self.router_port))
+            # set timeout to 1
+            connection.settimeout(1)
+            # wait for response
+            print('Client: Waiting for DATA')
+            response, sender = connection.recvfrom(1024)
+            data_packet = Packet.from_bytes(response)
+            print('Client: Received packet from ', sender)
+            print('Client: Received ', data_packet)
+            print('Client: Packet payload = ', data_packet.payload.decode('utf-8'))
+            # check if the packet is DATA (4)
+            if data_packet.packet_type == 4:
+                # reset timeout
+                connection.settimeout(None)
+                success = True
+                response_data = data_packet.payload.decode('utf-8')
+                # send ACK back
+                ack_packet = Packet(5, 5, self.peer_ip, self.server_port, 'ACK'.encode('utf-8'))
+                # send the packet to the router
+                print('Client: Sending ACK (DATA) to router')
+                connection.sendto(ack_packet.to_bytes(), (self.router_host, self.router_port))
+        except socket.timeout:
+            print('Client: No response after 1 second, request failed')
+        return success, response_data
+
+
 
 # python echoclient.py --routerhost localhost --routerport 3000 --serverhost localhost --serverport 8007
 
--- udp_core/httpf_udp.py
+++ udp_core/httpf_udp.py
@@ -1,5 +1,4 @@
 import argparse
-import ipaddress
 import socket
 from udp_core.packet import Packet
 
@@ -14,10 +13,20 @@ class HttpfUDP:
         connection.bind(('', self.server_port))
         print('Server is listening to ', self.server_port)
         while True:
-            is_handshaked = self.handle_handshake(connection)
+            # handshake with client
+            is_handshaked = False
+            request_data = None
+            while not is_handshaked:
+                is_handshaked, request_data = self.handle_handshake(connection)
+            # handle request
+            is_handled = False
+            while not is_handled:
+                is_handled = self.handle_request(connection, request_data)
 
     def handle_handshake(self, connection):
+        connection.settimeout(3600)
         data, sender = connection.recvfrom(1024)
+        print('---- Handshaking ----')
         success = False
         request_data = None
         # restore syn_packet that received from the client
@@ -29,28 +38,59 @@ class HttpfUDP:
         # check if the packet is SYN (1)
         if syn_packet.packet_type == 1:
             try:
-                # create SYN-ACK, where type = 2, seq_num = 2, payload = 'SYN-ACK'
+                # create SYN-ACK packet, where type = 2, seq_num = 2, payload = 'SYN-ACK'
                 print('Server: Creating SYN-ACK packet')
                 syn_ack_packet = Packet(2, 2, syn_packet.peer_ip_addr, syn_packet.peer_port, 'SYN-ACK'.encode('utf-8'))
                 print('Server: Sending SYN-ACK to router')
                 connection.sendto(syn_ack_packet.to_bytes(), sender)
-                #print("Server: Waiting for ACK")
-                #connection.settimeout(1)
-                #response, sender = connection.recvfrom(1024)
-                #ack_response = Packet.from_bytes(response)
-                #print('Server: Received packet from ', sender)
-                #print('Server: Received ', ack_response)
-                #print('Server: Packet payload = ', ack_response.payload.decode('utf-8'))
+                print("Server: Waiting for ACK")
+                connection.settimeout(3600)
+                response, sender = connection.recvfrom(1024)
+                ack_packet = Packet.from_bytes(response)
+                print('Server: Received packet from ', sender)
+                print('Server: Received ', ack_packet)
+                print('Server: Packet payload = ', ack_packet.payload.decode('utf-8'))
                 # check if the packet is ACK (3)
-                #if ack_response.packet_type == 3:
-                #    # reset timeout
-                #   connection.settimeout(None)
-                #    success = True
-                #    data = ack_response.payload.decode('utf-8')
+                if ack_packet.packet_type == 3:
+                    # reset timeout
+                    connection.settimeout(None)
+                    success = True
+                    request_data = {
+                        'packet': ack_packet,
+                        'sender': sender,
+                        'peer_ip_addr': syn_packet.peer_ip_addr,
+                        'peer_port': syn_packet.peer_port
+                    }
             except socket.timeout:
                 connection.settimeout(None)
                 print('Client: No response after 1 second, handshake failed.')
         return success, request_data
+
+    def handle_request(self, connection, request_data):
+        # do some magic here to handle data
+        print('---- Handling Request ----')
+        magic_data = request_data['packet'].payload.decode('utf-8')
+        magic_data_reurn = 'return data'
+        sender = request_data['sender']
+        success = False
+        try:
+            while True:
+                # create DATA packet, where type = 4, seq_num = 4, payload = data
+                print('Server: Creating DATA packet')
+                data_packet = Packet(4, 4, request_data['peer_ip_addr'], request_data['peer_port'], magic_data_reurn.encode('utf-8'))
+                print('Server: Sending DATA to router')
+                connection.sendto(data_packet.to_bytes(), sender)
+                # wait for confirmation
+                connection.settimeout(3600)
+                response, sender = connection.recvfrom(1024)
+                ack_packet = Packet.from_bytes(response)
+                if ack_packet.packet_type != 3:
+                    break
+            success = True
+        except socket.timeout:
+            connection.settimeout(None)
+            print('Client: No response after 1 second, handle request failed.')
+        return success
 
 
 # Usage python udp_server.py [--port port-number]
